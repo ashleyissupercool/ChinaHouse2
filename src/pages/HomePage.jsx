16db0bc1012109313b59e147fa1a87395bed2a6b
unknown <ashleyani1012@gmail.com>
2026-08-03 16:46:32 -0400

    Build header/footer components

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/layout/Header.jsx";
+import Footer from "../components/layout/Footer.jsx";
+
+function HomePage() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default HomePage;
---
+++ src/pages/MenuPage.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/layout/Header.jsx";
+import Footer from "../components/layout/Footer.jsx";
+
+function MenuPage() {
+  return(
+    <>
+    <Header />
+    <Footer />
+    </>
+  );
+} 
+export default MenuPage;
